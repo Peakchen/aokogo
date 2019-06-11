7dd289e8f1295399e57694be4e5a824269ff4ee1
Stefan <2572915286@qq.com>
2019-06-11 14:34:48 +0800

    update asynclock

--- src/common/ASyncLock/ASyncLock.go
+++ src/common/ASyncLock/ASyncLock.go
@@ -13,25 +13,24 @@ func NewAsyncLock(pools []redsync.Pool){
 	GRedsyncObj = redsync.New(pools)
 }
 
-func AddAsyncLock(key, Name string)bool{
+func AddAsyncLock(key, Name string){
 	lockid := key + ":" + Name
-	if _, ok := GAsynclock[lockid]; ok {
-		return false
+	if _, ok := GAsynclock[lockid]; !ok {
+		GAsynclock[lockid] = GRedsyncObj.NewMutex(lockid, 
+			redsync.SetExpiry(time.Duration(10*time.Second)),
+			redsync.SetRetryDelay(time.Duration(1*time.Second)))
 	}
-	GAsynclock[lockid] = GRedsyncObj.NewMutex(lockid, 
-						redsync.SetExpiry(time.Duration(10*time.Second)),
-						redsync.SetRetryDelay(time.Duration(1*time.Second)))
 	GAsynclock[lockid].Lock()
-	return true
+	return
 }
 
-func ReleaseAsyncLock(key, Name string) bool {
+func ReleaseAsyncLock(key, Name string) {
 	lockid := key + ":" + Name
-	if _, ok := GAsynclock[lockid]; ok {
-		return false
+	if _, ok := GAsynclock[lockid]; !ok {
+		return
 	}
 	GAsynclock[lockid].Unlock()
-	return true
+	return
 }
 
 
