267fa9959a10a15ef9c345d4cea82b13c7d16f2b
Stefan <2572915286@qq.com>
2019-12-19 15:53:21 +0800

    fix static make.
    add common and vendor static make.

--- make_GameServer.bat
+++ make_GameServer.bat
@@ -13,6 +13,17 @@ set GOARCH=amd64
 echo start install sever ...
 go install -gcflags " -N -l" GameServer
 
+rem cd %~dp0src\GameServer\
+
+rem for /r %%i in (*.go) do (
+rem	echo %%i
+rem	go tool compile -I %~dp0\pkg\windows_amd64 %%i
+rem )
+
+rem go tool link -o ../../bin/GameServer.exe -L %~dp0\pkg\windows_amd64 main.o
+
 echo make ok
 
 set GOPATH=%GOPATH_BAK%
+
+pause
---
+++ make_common_static.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set VERSION=v.0.1.0
+echo version=%VERSION%
+
+set GOPATH_BAK=%GOPATH%
+set GOPATH=%GOPATH%;%~dp0;%~dp0\aoko;
+
+set GOOS=windows
+set GOARCH=amd64
+
+cd %~dp0src\common\
+for /r %%i in (*.go) do (
+	echo %%i
+	go tool compile -I %~dp0\pkg\windows_amd64 %%i
+)
+
+echo make ok
+
+set GOPATH=%GOPATH_BAK%
+
+pause
