8104b90327120376ced6dac35a7dd3803446ce66
Stefan <2572915286@qq.com>
2020-01-19 11:18:56 +0800

    add encode for chinese txt.

--- src/common/utls/encoding.go
+++ src/common/utls/encoding.go
@@ -11,3 +11,10 @@ func GBKToUTF8(src string) string {
 func UTF8ToGBK(src string) string {
 	return mahonia.NewEncoder("gbk").ConvertString(src)
 }
+
+/*
+	for chinese encoding.
+*/
+func UTF8ToGB18030(src string) string {
+	return mahonia.NewDecoder("GB18030").ConvertString(src)
+}
